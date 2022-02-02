5dd6d6f986c704f9cc851912218dca571d1904d2
Francisco Sosa <franky.sosa@gmail.com>
2022-02-02 23:18:38 +0000

    Setup event listener to capture mint event data

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0xb16FCC6c227bAC8f39919326630a74C49e9C9d06";
+
 const App = () => {
 
   /*
@@ -42,6 +44,10 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+
+      // Setup listener. This is for the case where a user comes to the site
+      // and already had their wallet connected + authorized.
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -62,17 +68,75 @@ const App = () => {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       /*
-      * Boom! This should print out public address once we authorize Metamask.
+      * This should print out public address once we authorize Metamask.
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+
+      // Setup listener. This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onRinkeby = async () => {
+    console.log("Checking user is on Rinkeby")
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4"; 
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+          return false
+        } else if (chainId == rinkebyChainId) {
+          console.log("Rinkeby detected")
+          return true
+        }
+      } else {
+        console.log("No ethereum object.");
+      } 
+    } catch(error) {
+        console.log(error);
+      }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, mijoNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        connectedContract.on("NewMijoNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error)
     }
   }
 
   const askContractToMintNft = async () => {
-  const CONTRACT_ADDRESS = "0xb16FCC6c227bAC8f39919326630a74C49e9C9d06";
 
   try {
     const { ethereum } = window;
